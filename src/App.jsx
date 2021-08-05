bbb3f9841e5218821ae5ed07daf916ff6441f80e
kim_haaga <kim.haaga@live.se>
2021-08-05 12:46:25 +0200

    remove tests in app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,14 @@
-import { Typography } from '@material-ui/core'
-import React from 'react'
+import { Typography } from '@material-ui/core';
+import React from 'react';
 
 const App = () => {
   return (
     <>
-    <Typography variant="h1" component="h1" color="secondary">
-      Hello world
-    </Typography>
-    <Typography variant="h2" component="h2" color="primary">
-      Hello world
-    </Typography>
-    <Typography variant="h3" component="h3">
-      Hello world
-    </Typography>
-    <Typography variant="body1" component="p">
-      Hello world
-    </Typography>
+      <Typography variant='h1' component='h1' color='secondary'>
+        Hello world
+      </Typography>
     </>
-  )
-}
+  );
+};
 
-export default App
-
+export default App;
