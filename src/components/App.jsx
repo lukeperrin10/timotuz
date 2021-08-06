ee698b72b8dbbcbbf28cef95dc4a361442ae92b8
sevader14 <vsevolod.deriushkin@gmail.com>
2021-08-06 10:39:38 +0200

    adds simple header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
 import { Box } from '@material-ui/core';
-// import { Box, Container } from '@material-ui/core';
 import React from 'react';
-import Header from './Navigation/Header';
+import Header from './navigation/Header';
 
 const App = ({ component }) => {
   return (
     <>
       <Header />
-        <Box>{component}</Box>
-      {/* <Container maxWidth='xl' style={{ padding: '0px', margin: '0px' }}> */}
-        {/* <Box my={10}>{component}</Box> */}
-      {/* </Container> */}
+      <Box>{component}</Box>
     </>
   );
 };
---
+++ src/components/navigation/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {AppBar, Toolbar} from '@material-ui/core'
+
+const Header = () => {
+  return (
+    <AppBar>
+      <Toolbar>
+        Header
+      </Toolbar>
+    </AppBar>
+  )
+}
+
+export default Header
