cf60ab03463bc4f5d64c53bba96b29724ae4d43e
kim_haaga <kim.haaga@live.se>
2021-08-09 10:10:16 +0200

    Add test for contactus page

---
+++ cypress/integration/visitorCanSeeContactUsView.feature.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+describe('visitor can navigate see index view', () => {
+  beforeEach(() => {
+    cy.visit('/kontakta')
+  })
+
+  describe('can see contact us page', () => {
+    it('it is expected to show image on left', () => {
+      cy.get('[data-cy=image]')
+        .should('have.attr', 'alt')
+        .should('contain', 'Bild av stad från ovan')
+      cy.get('[data-cy=phone]').should('contain', 'Telefon Nummer')
+      cy.get('[data-cy=phone-number]').should('contain', '031-123-4567')
+      cy.get('[data-cy=email]').should('contain', 'E-post address')
+      cy.get('[data-cy=email-address]').should('contain', 'jakob@timotuz.se')
+    })
+  })
+})
---
+++ src/views/ContactUsView.jsx
@@ -0,0 +1,13 @@
+import { Box } from '@material-ui/core'
+import React from 'react'
+
+
+const ContactUsView = () => {
+  return (
+    <Box>
+      
+    </Box>
+  )
+}
+
+export default ContactUsView
