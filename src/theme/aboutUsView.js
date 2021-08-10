bacad92755b505228db6da0bc6261e7319a912dc
lukeperrin10 <lukeperrin27@gmail.com>
2021-08-10 14:23:34 +0200

    adds styling to about us page removes un-needed test file

--- src/theme/aboutUsView.js
+++ src/theme/aboutUsView.js
@@ -1,23 +1,44 @@
 import { makeStyles } from '@material-ui/core'
 
 const aboutUsStyles = makeStyles((theme) => ({
-  containerBox: {
-    marginTop: '100px',
-    padding: '70px 200px 50px 200px',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+  pageBox: {
+    [theme.breakpoints.up('xs')]: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    [theme.breakpoints.up('lg')]: {
+      marginTop: '70px',
+      marginBottom: '20px',
+      padding: '0',
+    },
   },
 
   header: {
-    marginTop: '20px',
     marginBottom: '50px',
   },
 
-  headerMission: {
-    marginTop: '50px',
-    marginBottom: '50px'
-  }, 
+  textContainer: {
+    [theme.breakpoints.up('xs')]: {
+      textAlign: 'center',
+      margin: '100px 0',
+      padding: '70px 30px 70px 30px',
+    },
+    [theme.breakpoints.up('lg')]: {
+      padding: '0 400px',
+    },
+  },
+
+  textContainerDark: {
+    [theme.breakpoints.up('xs')]: {
+      textAlign: 'center',
+      padding: '150px 30px 150px 30px',
+      backgroundColor: '#ddd',
+    },
+    [theme.breakpoints.up('lg')]: {
+      padding: '70px 400px',
+    },
+  },
 }))
 
 export default aboutUsStyles
--- src/views/AboutUsView.jsx
+++ src/views/AboutUsView.jsx
@@ -5,42 +5,61 @@ import aboutUsStyles from '../theme/aboutUsView'
 const AboutUsView = () => {
   const classes = aboutUsStyles()
   return (
-    <Box className={classes.containerBox}>
-      <Box className={classes.header}>
-        <Typography variant="h4">Om oss</Typography>
+    <Box className={classes.pageBox}>
+      <Box className={classes.textContainer}>
+        <Box className={classes.header}>
+          <Typography data-cy="about-us-header" variant="h4">
+            Om Oss
+          </Typography>
+        </Box>
+        <Box className={classes.text}>
+          <Typography data-cy="about-us-main" variant="body1">
+            Timotuz fastigheter är ett erfaret fastighetsbolag i södra Sverige
+            med hjärtat i myllan. Vårt fastighetsbestånd är koncentrerat till
+            orter i Skåneregionen och utgörs i huvudsak av bostäder och
+            industrifastigheter. Vi växer genom förvärv av fastigheter med
+            utvecklingspotential samt nyproduktion av primärt
+            bostadsfastigheter. Fina relationer med våra hyresgäster och goda
+            kassaflöden har skapat förutsättningar för vår regionalt fokuserade
+            fastighetsutveckling och ligger till grund för vår stabila tillväxt.
+            För att skapa värden för både städer och människor jobbar vi varsamt
+            och miljösmart med såväl renovering av befintliga fastigheter som
+            med framtagande av nya bostäder. Bolaget grundades 2014 och verkar
+            genom fastigheter och utvecklingsprojekt i Malmö, Ystad, Helsingborg
+            och Limhamn.
+          </Typography>
+        </Box>
       </Box>
-      <Typography variant="body1">
-        Timotuz fastigheter är ett erfaret fastighetsbolag i södra Sverige med
-        hjärtat i myllan. Vårt fastighetsbestånd är koncentrerat till orter i
-        Skåneregionen och utgörs i huvudsak av bostäder och industrifastigheter.
-        Vi växer genom förvärv av fastigheter med utvecklingspotential samt
-        nyproduktion av primärt bostadsfastigheter. Fina relationer med våra
-        hyresgäster och goda kassaflöden har skapat förutsättningar för vår
-        regionalt fokuserade fastighetsutveckling och ligger till grund för vår
-        stabila tillväxt. För att skapa värden för både städer och människor
-        jobbar vi varsamt och miljösmart med såväl renovering av befintliga
-        fastigheter som med framtagande av nya bostäder. Bolaget grundades 2014
-        och verkar genom fastigheter och utvecklingsprojekt i Malmö, Ystad,
-        Helsingborg och Limhamn.
-      </Typography>
-      <Box className={classes.headerMission}>
-        <Typography variant="h4">Vår Mission</Typography>
+      <Box className={classes.textContainerDark}>
+        <Box className={classes.header}>
+          <Typography data-cy="about-us-mission-header" variant="h4">
+            Vår Mission
+          </Typography>
+        </Box>
+        <Box className={classes.text}>
+          <Typography data-cy="about-us-mission" variant="body1">
+            Timotuz skall skapa platser för skåningar att växa och trivas på. Vi
+            gör det genom att tillgodose bostads- och lokalbehov idag, men med
+            ena foten in i morgondagens teknik och miljö. Vi ska hitta eller
+            skapa fastigheter med stor värdepotential och utveckla dessa för ett
+            lönsamt ägande och en hållbar framtid.
+          </Typography>
+        </Box>
       </Box>
-      <Typography>
-        Timotuz skall skapa platser för skåningar att växa och trivas på. Vi gör
-        det genom att tillgodose bostads- och lokalbehov idag, men med ena foten
-        in i morgondagens teknik och miljö. Vi ska hitta eller skapa fastigheter
-        med stor värdepotential och utveckla dessa för ett lönsamt ägande och en
-        hållbar framtid.
-      </Typography>
-      <Box className={classes.headerMission}>
-        <Typography variant="h4">Vår Vision</Typography>
+      <Box className={classes.textContainer}>
+        <Box className={classes.header}>
+          <Typography data-cy="about-us-vision-header" variant="h4">
+            Vår Vision
+          </Typography>
+        </Box>
+        <Box className={classes.text}>
+          <Typography data-cy="about-us-vision" variant="body1">
+            Med den regionalt förankrade småföretagarens lyhördhet och passion
+            skapar Timotuz rätt fastigheter, och vårdar dem med storbolagets
+            långsiktighet och branscherfarenhet.
+          </Typography>
+        </Box>
       </Box>
-      <Typography>
-        Med den regionalt förankrade småföretagarens lyhördhet och passion
-        skapar Timotuz rätt fastigheter, och vårdar dem med storbolagets
-        långsiktighet och branscherfarenhet.
-      </Typography>
     </Box>
   )
 }
