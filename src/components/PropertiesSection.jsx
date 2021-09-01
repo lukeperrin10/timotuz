fe7c30d8811f0a121720fa941fb09cb9d63d471f
kim_haaga <kim.haaga@live.se>
2021-09-01 16:53:03 +0200

    Add grid layout

--- src/components/PropertiesSection.jsx
+++ src/components/PropertiesSection.jsx
@@ -1,47 +1,90 @@
 import React from 'react'
-import { Box, Typography, Button } from '@material-ui/core'
+import {
+  Box,
+  Typography,
+  CardMedia,
+  Button,
+  Grid,
+  Container,
+} from '@material-ui/core'
 import { Link } from 'react-router-dom'
-import properties from '../data/fixtures/properties'
-import PropertyCard from './PropertyCard'
-import Slider from './Slider'
-import propertySectionStyle from '../theme/themePropertiesSection'
+import propertyGridStyle from '../theme/themePropertiesGrid'
+import helsingborg from '../assets/images/Helsingborg/1.jpg'
+import fredrik from '../assets/images/Pågående_Ystad/02.jpg'
+import strutsen from '../assets/images/Limhamn/1.JPG'
 
 const PropertiesSection = () => {
-  const classes = propertySectionStyle()
-  const property = properties.map((property, id) => {
-    return <PropertyCard property={property} key={id} />
-  })
+  const classes = propertyGridStyle()
 
   return (
-    <>
-      <Box className={classes.headerTextContainer}>
-        <Typography
-          className={classes.header}
-          data-cy="properties-header"
-          color="secondary"
-          variant="h4"
-          gutterBottom
-        >
-          Våra fastigheter
-        </Typography>
-        <Typography variant="subtitle1">
-          I dagsläget förvaltar vi fastigheter i Helsingborg, Malmö, Ystad och
-          Limhamn och vår fastighetsportfölj utökas ständigt.
-        </Typography>
-      </Box>
-      <Slider>{property}</Slider>    
-      <Box className={classes.button}>
-        <Button
-          data-cy="våra-fastigheter-btn"
-          variant="outlined"
-          color="secondary"
-          component={Link}
-          to="/vara_fastigheter"
-        >
-          Alla våra fastigheter
-        </Button>
-      </Box>
-      </>
+    <Container className={classes.gridContainer}>
+      <Grid className={classes.gridRow} container>
+        <Grid className={classes.gridItem} item>
+          <Container className={classes.outerBox}>
+            <Container className={classes.innerBox}>
+              <Typography variant='body1' className={classes.firstText}>
+                Söker du lokal eller bostad?
+              </Typography>
+              <Typography variant='body2' className={classes.phoneNumber}>
+                040 - 68 59 400
+              </Typography>
+            </Container>
+          </Container>
+        </Grid>
+        <Grid className={classes.gridItem} item>
+          <CardMedia
+            className={classes.image}
+            component={Link}
+            to='/vara_fastigheter'
+            image={strutsen}
+            data-cy='helsinborg-img'
+            alt='Huset strutsen från ovan'>
+            <Box className={classes.shadowBox}></Box>
+          </CardMedia>
+        </Grid>
+      </Grid>
+      <Grid className={classes.gridRow} container>
+        <Grid className={classes.gridItemCenter} item>
+          <CardMedia
+            className={classes.image}
+            component={Link}
+            to='/vara_fastigheter'
+            image={fredrik}
+            data-cy='helsinborg-img'
+            alt='Illustration av folk på en innergård'>
+            <Box className={classes.shadowBoxCenter}></Box>
+          </CardMedia>
+        </Grid>
+      </Grid>
+      <Grid className={classes.gridRow} container>
+        <Grid className={classes.gridItem} item>
+          <CardMedia
+            className={classes.image}
+            component={Link}
+            to='/vara_fastigheter'
+            image={helsingborg}
+            data-cy='helsinborg-img'
+            alt='Industri lokal med röda portar'>
+            <Box className={classes.shadowBox}></Box>
+          </CardMedia>
+        </Grid>
+        <Grid className={classes.gridItem} item>
+          <Container className={classes.outerBox}>
+            <Button
+              className={classes.button}
+              data-cy='våra-fastigheter-btn'
+              variant='outlined'
+              color='common'
+              component={Link}
+              to='/vara_fastigheter'>
+              <Typography className={classes.buttonText} variant='button'>
+                Visa flera fastigheter
+              </Typography>
+            </Button>
+          </Container>
+        </Grid>
+      </Grid>
+    </Container>
   )
 }
 
---
+++ src/theme/themePropertiesGrid.js
@@ -0,0 +1,135 @@
+import { makeStyles } from '@material-ui/core'
+
+const propertyGridStyle = makeStyles((theme) => ({
+  gridContainer: {
+    [theme.breakpoints.up('xs')]: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      marginTop: '56px',
+      marginBottom: '114px',
+    },
+    [theme.breakpoints.up('lg')]: {
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginTop: '56px',
+      marginBottom: '114px',
+    },
+  },
+  gridRow: {
+    [theme.breakpoints.up('xs')]: {
+      display: 'flex',
+      flexDirection: 'column',
+      flexWrap: 'nowrap',
+      alignItems: 'center',
+    },
+    [theme.breakpoints.up('lg')]: {},
+  },
+  gridItem: {
+    [theme.breakpoints.up('xs')]: {
+      width: '346px',
+      height: '248px',
+      margin: '27px',
+      justifyContent: 'center',
+    },
+    [theme.breakpoints.up('lg')]: {
+      width: '346px',
+      height: '248px',
+      margin: '27px',
+      justifyContent: 'center',
+    },
+  },
+
+  gridItemCenter: {
+    [theme.breakpoints.up('xs')]: {
+      width: '100vw',
+      margin: '27px',
+    },
+    [theme.breakpoints.up('sm')]: {
+      width: '346px',
+      height: '550px',
+    },
+    [theme.breakpoints.up('lg')]: {
+      width: '346px',
+      height: '550px',
+    },
+  },
+
+  centerText: {
+    display: 'flex',
+    flexDirection: 'column',
+    textAlign: 'center',
+  },
+
+  outerBox: {
+    display: 'flex',
+    flexDirection: 'column',
+    textAlign: 'center',
+    backgroundColor: theme.palette.primary.main,
+    width: '100%',
+    height: '100%',
+  },
+
+  innerBox: {
+    textAlign: 'center',
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.primary.contrastText,
+    border: 'solid #fff 2px',
+    margin: 'auto',
+    padding: '65px 35px',
+  },
+
+  firstText: {
+    marginBottom: '18px',
+  },
+
+  phoneNumber: {
+    marginTop: '18px',
+    fontSize: '1.5rem',
+  },
+
+  image: {
+    width: '100%',
+    height: '100%',
+    objectFit: 'cover',
+  },
+
+  shadowBox: {
+    width: '346px',
+    height: '248px',
+    position: 'absolute',
+    backgroundColor: 'rgba(15, 15, 15, 0.2)',
+  },
+
+  shadowBoxCenter: {
+    [theme.breakpoints.up('xs')]: {
+      width: '100vw',
+      position: 'absolute',
+      backgroundColor: 'rgba(15, 15, 15, 0.2)',
+    },
+    [theme.breakpoints.up('sm')]: {
+      width: '346px',
+      height: '550px',
+    },
+  },
+
+  button: {
+    [theme.breakpoints.up('xs')]: {
+      display: 'flex',
+      justifyContent: 'center',
+      border: 'solid #fff 2px',
+      borderRadius: '0px',
+      width: '191px',
+      height: '50px',
+      margin: 'auto',
+    },
+  },
+  buttonText: {
+    [theme.breakpoints.up('xs')]: {
+      color: theme.palette.primary.contrastText,
+    },
+  },
+}))
+
+export default propertyGridStyle
