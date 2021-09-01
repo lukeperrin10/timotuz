115b027c107aaf0009cbab42e106f33ffc9fd56d
kim_haaga <kim.haaga@live.se>
2021-09-01 13:20:51 +0200

    remove unneccesary content

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Box, CardMedia, Grid, Typography } from '@material-ui/core'
-import overview from '../assets/images/overview.jpeg'
+import { Box, CardMedia} from '@material-ui/core'
+import overview from '../assets/images/Pågående_Ystad/01.jpg'
 import heroSectionStyle from '../theme/heroSection'
 
 
@@ -16,57 +16,9 @@ const HeroSection = () => {
         alt="Bird view of a small town"
       />
       <Box data-cy="shadow-box" className={classes.shadowBox}>
-        <Box className={classes.shadowBoxContent}>
-          <Typography
-            className={classes.informationText}
-            variant="h2"
-            data-cy="information-text"
-          >
-            Trygga fastigheter i södra sverige
-          </Typography>
-        </Box>
+        
       </Box>
-      <Box
-        data-cy="information-bar"
-        className={classes.redInfoBox}
-        bgcolor="secondary.main"
-        color="secondary.contrastText"
-      >
-        <Grid container spacing={0} className={classes.infoGrid}>
-          <Grid className={classes.dataColumn} data-cy="column-0" item xs={2}>
-            <Typography className={classes.dataText} variant="h4">
-              114
-            </Typography>
-            <Typography variant="subtitle2" gutterBottom>
-              Lorem ipsum
-            </Typography>
-          </Grid>
-          <Grid className={classes.dataColumn} data-cy="column-1" item xs={2}>
-            <Typography className={classes.dataText} variant="h4">
-              32
-            </Typography>
-            <Typography variant="subtitle2" gutterBottom>
-              Lorem ipsum
-            </Typography>
-          </Grid>
-          <Grid className={classes.dataColumn} data-cy="column-2" item xs={2}>
-            <Typography className={classes.dataText} variant="h4">
-              2
-            </Typography>
-            <Typography variant="subtitle2" gutterBottom>
-              Lorem ipsum
-            </Typography>
-          </Grid>
-          <Grid className={classes.dataColumn} data-cy="column-3" item xs={2}>
-            <Typography className={classes.dataText} variant="h4">
-              Ja
-            </Typography>
-            <Typography variant="subtitle2" gutterBottom>
-              Lorem ipsum
-            </Typography>
-          </Grid>
-        </Grid>
-      </Box>
+     
     </Box>
   )
 }
