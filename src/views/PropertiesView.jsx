db1027c7906d580cfe2592ef759e8662e6a7897f
sevader14 <vsevolod.deriushkin@gmail.com>
2021-09-01 18:09:27 +0200

    resolves merge conflicts

--- src/views/PropertiesView.jsx
+++ src/views/PropertiesView.jsx
@@ -30,28 +30,8 @@ const PropertiesView = () => {
         open={slideShow}
         setSlideShow={setSlideShow}
       />
-      <Box className={classes.headerTextContainer}>
-        <Typography
-          className={classes.header}
-          data-cy='properties-header'
-          color='secondary'
-          variant='h4'
-          gutterBottom>
-          Bostäder
-        </Typography>
-        <Typography variant='subtitle1'>
-          Timotuz Fastigheter hyr ut och förvaltar bostäder i vackra och mysiga
-          lägen runt om i Skåneregionen. Vi bedriver förvaltningen i egen regi
-          med samarbetar med lokala entreprenörer som underleverantörer. Vi
-          jobbar proaktivt och långsiktigt med vår förvaltning där en
-          medvetenhet om miljö och etiska principer alltid vägs in i de beslut
-          vi tar för att möta hyresgästernas behov. Söker du en ny bostad? Vi
-          vill gärna veta hur vi kan hjälpa dig!
-        </Typography>
-      </Box>
+      <FastigheterHero />
       <Container maxWidth='xl' className={classes.viewContainer}>
-      <FastigheterHero/>
-      <Container maxWidth="xl" className={classes.viewContainer}>
         <Grid container>{property}</Grid>
       </Container>
       <Box className={classes.headerTextContainer}>
